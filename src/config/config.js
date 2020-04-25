15cfc7d6cc4b2ba0523c6129ceef9b46da9b680e
Enmanuel-Leon <45642674+enmanuel23x@users.noreply.github.com>
2020-04-25 12:42:44 -0400

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    backURL: "http://localhost:4080",
+    backURL: "http://10.48.13.156:4080",
     clientId:"808436269199-clfno654lulirtkmn17p1g6o5klks9g6.apps.googleusercontent.com",
-    email:""
+    email:"anarvaez@intelix.biz"
 };
