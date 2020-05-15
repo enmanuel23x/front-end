9322f662d0566326f2a56a8438052f6a0ee3590a
Enmanuel-Leon <45642674+enmanuel23x@users.noreply.github.com>
2020-05-15 18:25:45 -0400

    Update

--- src/config/keycloak.js
+++ src/config/keycloak.js
@@ -1,3 +1,3 @@
 import Keycloak from 'keycloak-js';
-const keycloak= new Keycloak({ url: 'http://localhost:8080/auth/', realm: 'Google-Auth', clientId: 'google' });
+const keycloak= new Keycloak({ url: 'http://10.48.13.152/auth/', realm: 'MapeoConocimientos', clientId: 'google' });
 export default keycloak;
