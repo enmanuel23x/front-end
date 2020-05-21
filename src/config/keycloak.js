bc87fa8f90e986c99cf18ca64aab0eaae8021d35
Alejandro <asanchezb@intelix.biz>
2020-05-21 10:21:58 -0400

    security certs added

--- src/config/keycloak.js
+++ src/config/keycloak.js
@@ -1,3 +1,3 @@
 import Keycloak from 'keycloak-js';
-const keycloak= new Keycloak({ url: 'http://10.48.13.152.nip.io/auth/', realm: 'MapaConocimientos', clientId: 'google' });
+const keycloak= new Keycloak({ url: 'https://10.48.13.152.nip.io/auth/', realm: 'MapaConocimientos', clientId: 'google' });
 export default keycloak;
