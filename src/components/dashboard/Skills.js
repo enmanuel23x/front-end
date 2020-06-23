49abd22337661b68774917d299e0bdfddee8a338
Alejandro <aesbetancourt@outlook.com>
2020-06-23 13:02:12 -0400

    Functions almost ready (Skills, Users)

--- src/components/dashboard/Skills.js
+++ src/components/dashboard/Skills.js
@@ -1,31 +1,10 @@
 import React, {useEffect, useState} from 'react';
 import 'antd/dist/antd.css';
 import {Table, Space, Button} from 'antd';
+import Swal from 'sweetalert2'
 const axios = require('axios').default;
 
-const columns = [
-    {
-        title: 'Conocimiento',
-        dataIndex: 'name',
-        key: 'name',
-    },
-    {
-        title: 'Categoria',
-        dataIndex: 'category',
-        key: 'category',
-    },
-    {
-        title: 'Operación',
-        key: 'action',
-        width: "20%",
-        render: (text, record) => (
-            <Space size="middle">
-                <a>Eliminar</a>
-                <a>Editar</a>
-            </Space>
-        ),
-    },
-];
+
 
 async function getData() {
     let skills = []
@@ -35,7 +14,7 @@ async function getData() {
             for (let i = 0; i <  response.data.length; i++) {
                 await skills.push({
                     name: response.data[i].name,
-                    category: response.data[i].category_id,
+                    category: response.data[i].category_id
                 })
             }
         })
@@ -49,25 +28,143 @@ async function getData() {
         });
     return skills
 }
-
-
 const Skills = () => {
     const [data, setData] = useState([]);
 
+    async function fillTable(){
+        let info = await getData()
+        console.log(info)
+        setData(info)
+    }
     useEffect(() => {
-        async function fillTable(){
-            let info = await getData()
-            console.log(info)
-            setData(info)
-        }
         fillTable()
-
-
     }, [])
+
+    function deleteRecord(id, name){
+        Swal.fire({
+            title: "¿Quieres eliminar la habilidad "+name+"?",
+            text: "Una vez eliminado no se podra recuperar",
+            icon: "warning",
+            buttons: true,
+            showCancelButton: true,
+            confirmButtonText: 'Eliminar',
+            cancelButtonText: 'Cancelar',
+            dangerMode: true,
+        })
+            .then((willDelete) => {
+                if (willDelete.value) {
+                    Swal.fire({
+                        title:"La habilidad fue eliminada!",
+                        icon: "success",
+                    })
+                        .then((data) =>{
+                            axios.delete('resource/skills/'+id)
+                                .then( function (response) {
+                                    console.log(response)
+                                    fillTable()
+                                });
+                        });
+                } else {
+                    Swal.fire("La habilidad no fue eliminada!");
+                }
+            });
+    }
+
+
+    async function createSkill(){
+        let skills = {}
+        await axios.get('/resource/categories')
+            .then(async function (response) {
+                // handle success
+                for (let i = 0; i <  response.data.length; i++) {
+                    skills[response.data[i].id] = response.data[i].name
+                }
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                console.log("Skills fetched")
+                // always executed
+            });
+
+
+
+        Swal.mixin({
+            input: 'text',
+            confirmButtonText: 'Siguiente &rarr;',
+            cancelButtonText: 'Cancelar',
+
+            showCancelButton: true,
+            progressSteps: ['1', '2']
+        }).queue([
+            {
+                title: 'Conocimiento',
+                text: 'Ingrese la habilidad asociada'
+            },
+            {
+                title: 'Categoria',
+                text: 'Elija la Categoria',
+                input: 'select',
+                inputOptions: skills
+            },
+        ]).then((result) => {
+            if (result.value) {
+                const answers = (result.value)
+
+                axios.put('resource/skills', {
+                    name: answers[0],
+                    category_id: parseInt(answers[1])
+                })
+                    .then(response => {
+                        // console.log(response);
+                        fillTable()
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'Nuevo Conocimiento añadido',
+                    showConfirmButton: false,
+                    timer: 1500
+                })
+
+            }
+        })
+    }
+    const columns = [
+        {
+            title: 'Conocimiento',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Categoria',
+            dataIndex: 'category',
+            key: 'category',
+        },
+        {
+            title: 'Operación',
+            key: 'action',
+            width: "20%",
+            render: (text, record) => (
+                <Space size="middle">
+                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                    <a onClick={() => deleteRecord(record.id,record.name)}>Eliminar</a>
+                    <a>Editar</a>
+
+                </Space>
+            ),
+        },
+    ];
+
     return (
         <div>
             <Button
-                // onClick={}
+                onClick={createSkill}
                 type="primary"
                 style={{
                     marginBottom: 16,
--- src/components/dashboard/Users.js
+++ src/components/dashboard/Users.js
@@ -1,38 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import 'antd/dist/antd.css';
 import {Button, Space, Table} from 'antd';
+import { data } from 'jquery';
+import Swal from 'sweetalert2';
 const axios = require('axios').default;
-
-
-const columns = [
-    {
-        title: 'Nombre',
-        dataIndex: 'name',
-    },
-    {
-        title: 'Email',
-        dataIndex: 'email',
-        align: 'right',
-    },
-    {
-        title: 'Grupo',
-        dataIndex: 'group',
-        align: 'right',
-    },
-    {
-        title: 'Operación',
-        key: 'action',
-        render: (text, record) => (
-            <Space size="small">
-                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                <a>Eliminar</a>
-                {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                <a>Editar</a>
-            </Space>
-        ),
-    },
-];
-
 
 
 async function getData() {
@@ -44,7 +15,8 @@ async function getData() {
                 await users.push({
                     name: response.data[i].full_name,
                     email: response.data[i].email,
-                    group: response.data[i].group_id
+                    group: response.data[i].group_id,
+                    id: response.data[i].id
                 })
             }
         })
@@ -63,39 +35,94 @@ async function getData() {
 
 const Users = () =>  {
     const [data, setData] = useState([]);
-
+    async function fillTable(){
+        let info = await getData()
+        console.log(info)
+        setData(info)
+    }
     useEffect(() => {
-        async function fillTable(){
-            let info = await getData()
-            console.log(info)
-            setData(info)
-        }
         fillTable()
-
-
     }, [])
 
+    const columns = [
+        {
+            title: 'Nombre',
+            dataIndex: 'name',
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            align: 'right',
+        },
+        {
+            title: 'Grupo',
+            dataIndex: 'group',
+            align: 'right',
+        },
+        {
+            title: 'Operación',
+            key: 'action',
+            render: (text, record) => (
+                <Space size="small">
+                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                    <a onClick={() => deleteRecord(record.id,record.name)}>Eliminar</a>
+                    {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                    <a>Editar</a>
+                </Space>
+            ),
+        },
+    ];
 
 
+    function deleteRecord(id, name){
+        Swal.fire({
+            title: "¿Quieres eliminar al usuario "+name+"?",
+            text: "Una vez eliminado no se podra recuperar",
+            icon: "warning",
+            buttons: true,
+            showCancelButton: true,
+            confirmButtonText: 'Eliminar',
+            cancelButtonText: 'Cancelar',
+            dangerMode: true,
+        })
+            .then((willDelete) => {
+                if (willDelete.value) {
+                    Swal.fire({
+                        title:"El usuario fue eliminado!",
+                        icon: "success",
+                    })
+                        .then((data) =>{
+                            axios.delete('resource/users/'+id)
+                                .then( function (response) {
+                                    console.log(response)
+                                    fillTable()
+                                });
+                        });
+                } else {
+                    Swal.fire("El usuario no fue eliminado!");
+                }
+            });
+    }
 
-        return(
-            <div>
-                <Button
-                    type="primary"
-                    style={{
-                        marginBottom: 16,
-                    }}
-                >
-                    Agregar Usuario Nuevo
-                </Button>
 
-                <Table
-                    columns={columns}
-                    dataSource={data}
-                    bordered
-                />
-            </div>
-        )
+    return(
+        <div>
+            <Button
+                type="primary"
+                style={{
+                    marginBottom: 16,
+                }}
+            >
+                Agregar Usuario Nuevo
+            </Button>
+
+            <Table
+                columns={columns}
+                dataSource={data}
+                bordered
+            />
+        </div>
+    )
 
 };
 
